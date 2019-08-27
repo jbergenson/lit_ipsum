f66a2bffdf7402e4789934a5d45a245ed04f0280
Jac Bergenson <18683052+bergbergs@users.noreply.github.com>
2019-08-27 17:48:16 -0400

    Formatting

--- lib/lit_ipsum/poe/raven.rb
+++ lib/lit_ipsum/poe/raven.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
 module LitIpsum
-    class Poe
-      class Raven < Base
-        FILENAME = 'texts/poe/the-raven.txt'
+  class Poe
+    class Raven < Base
+      FILENAME = 'texts/poe/the-raven.txt'
   
-        class << self
-          def sentences(count, max_sentence = 0, filename = FILENAME)
-            super(count, max_sentence, filename)
-          end
-  
-          def words(count, filename = FILENAME)
-            super(count, filename)
-          end
+      class << self
+        def sentences(count, max_sentence = 0, filename = FILENAME)
+          super(count, max_sentence, filename)
+        end
+
+        def words(count, filename = FILENAME)
+          super(count, filename)
         end
       end
     end
   end
-  
+end
