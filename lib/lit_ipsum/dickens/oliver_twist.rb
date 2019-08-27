ee78b5265c62608520934bbec9dadf629d7d5427
Jac Bergenson <18683052+bergbergs@users.noreply.github.com>
2019-08-27 17:46:10 -0400

    Add newline and change to 2 spaces: oliver_twist.rb

--- lib/lit_ipsum/dickens/oliver_twist.rb
+++ lib/lit_ipsum/dickens/oliver_twist.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 module LitIpsum
-    class Dickens
-        class OliverTwist < Base
-            FILENAME = 'texts/dickens/oliver-twist.txt'
+  class Dickens
+    class OliverTwist < Base
+      FILENAME = 'texts/dickens/oliver-twist.txt'
 
-            class << self
-                def sentences(count, max_sentence = 0, filename = FILENAME)
-                    super(count, max_sentence, filename)
-                end
-
-                def words(count, filename = FILENAME)
-                    super(count, filename)
-                end
-            end
+      class << self
+        def sentences(count, max_sentence = 0, filename = FILENAME)
+          super(count, max_sentence, filename)
         end
+
+        def words(count, filename = FILENAME)
+          super(count, filename)
+        end
+      end
     end
+  end
 end
